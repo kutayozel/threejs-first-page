0d0ec403531e25edb9ab653a1c753a205e018ee9
Kharza <zelkutay@gmail.com>
2022-06-04 01:45:21 +0300

    rotating torus

---
+++ main.js
@@ -0,0 +1,40 @@
+import './style.css'
+
+import * as THREE from 'three'
+
+// We need always three objects
+//1.Scene 2.Camera 3.Renderer
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera
+(75, window.innerWidth/window.innerHeight, 0.1, 1000) //fov, aspect rat,view frustum
+
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg')
+})
+
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+camera.position.setZ(30)
+
+renderer.render(scene, camera)
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
+const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true})
+const torus = new THREE.Mesh(geometry, material)
+
+scene.add(torus)
+
+function animate() {
+  requestAnimationFrame(animate) //tells we want to perform an animation
+  
+  torus.rotation.x += 0.01
+  torus.rotation.y += 0.005
+  torus.rotation.z += 0.01
+  
+  renderer.render(scene, camera)
+}
+
+animate()
